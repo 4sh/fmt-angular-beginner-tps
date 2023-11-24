dffacc869d8d4e2fbeaffb778b7c799849b2c97c
mcastets <mathieu.castets@4sh.fr>
2023-11-24 11:32:50 +0100

    feat: set up wildcard routes with redirect to /cellar

--- frontend/src/app/app-root.routes.ts
+++ frontend/src/app/app-root.routes.ts
@@ -5,5 +5,9 @@ export const routes: Routes = [
         path: '',
         redirectTo: '/cellar',
         pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: '/cellar'
     }
 ];
