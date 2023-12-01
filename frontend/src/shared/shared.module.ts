626e57ebe3f2ec00c1b599aa2e0786bc1b6b9c51
mcastets <mathieu.castets@4sh.fr>
2023-12-01 12:10:45 +0100

    feat: add ToastService to SharedModule

--- frontend/src/shared/shared.module.ts
+++ frontend/src/shared/shared.module.ts
@@ -1,6 +1,8 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FilterByPipe} from './pipes/filter-by.pipe';
+import {ToastrModule} from 'ngx-toastr';
 
 const pipes: unknown[] = [
     FilterByPipe
@@ -11,7 +13,12 @@ const pipes: unknown[] = [
         pipes
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        BrowserAnimationsModule,
+        ToastrModule.forRoot({
+            positionClass: 'toast-bottom-right',
+            progressBar: true,
+        }),
     ],
     exports: [
         pipes
