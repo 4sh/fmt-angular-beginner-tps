45ead96f4c18b0f505e16982b73908fd987a3555
mcastets <mathieu.castets@4sh.fr>
2023-12-01 12:10:45 +0100

    feat: add ToastService to SharedModule

---
+++ frontend/src/shared/services/toast.service.ts
@@ -0,0 +1,31 @@
+import {Injectable} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {ToastrService} from 'ngx-toastr';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ToastService {
+
+    constructor(private toastr: ToastrService,
+                private translateService: TranslateService) {
+    }
+
+    public success(messageKey: string, messageParams?: MessageParams): void {
+        this.toastr.success(this.translateService.instant(messageKey, messageParams));
+    }
+
+    public info(messageKey: string, messageParams?: MessageParams): void {
+        this.toastr.info(this.translateService.instant(messageKey, messageParams));
+    }
+
+    public warning(messageKey: string, messageParams?: MessageParams): void {
+        this.toastr.warning(this.translateService.instant(messageKey, messageParams));
+    }
+
+    public error(messageKey: string, messageParams?: MessageParams): void {
+        this.toastr.error(this.translateService.instant(messageKey, messageParams));
+    }
+}
+
+export type MessageParams = Record<string, string | number>;
