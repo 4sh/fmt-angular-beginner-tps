d49f5aa8a377ecbdf21f059eb9de7ad73fc56790
Elian ORIOU <elian.oriou@4sh.fr>
2024-01-12 15:58:06 +0100

    v2/tp/03 - solution

--- frontend/src/app/features/auth/pages/login-page/login-page.component.ts
+++ frontend/src/app/features/auth/pages/login-page/login-page.component.ts
@@ -13,8 +13,13 @@ import {FormsModule} from '@angular/forms';
 export class LoginPageComponent {
     public login?: string;
     public password?: string;
+    public authStatus?: 'success' | 'error';
 
     public authenticate(): void {
-        // TODO
+        if ('test' === this.login && 'test' === this.password) {
+            this.authStatus = 'success';
+        } else {
+            this.authStatus = 'error';
+        }
     }
 }
