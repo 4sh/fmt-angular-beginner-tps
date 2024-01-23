89f1ec0f341b9163ae7897f3fa3d21e65b9d49ab
Elian ORIOU <elian.oriou@4sh.fr>
2024-01-23 17:41:35 +0100

    v1/tp/09 - solution

--- frontend/src/app/components/app-header/app-header.component.ts
+++ frontend/src/app/components/app-header/app-header.component.ts
@@ -1,14 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService, OptionalUserIdentity} from '../../modules/auth/services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-header',
     templateUrl: './app-header.component.html',
     styleUrl: './app-header.component.scss'
 })
-export class AppHeaderComponent { // TODO
-    // TODO
+export class AppHeaderComponent implements OnInit{
+    public currentUserIdentity: OptionalUserIdentity;
+
+    constructor(private authService: AuthService,
+                private router: Router) {
+    }
+
+    ngOnInit(): void {
+        this.authService.getCurrentUserIdentity()
+            .subscribe(currentUserIdentity => this.currentUserIdentity = currentUserIdentity);
+    }
 
     public logout(): void {
-        // TODO
+        this.authService
+            .logout()
+            .subscribe(() => this.router.navigate(['/auth']));
     }
 }
--- frontend/src/app/modules/cellar/pages/bottles-list-page/bottles-list-page.component.ts
+++ frontend/src/app/modules/cellar/pages/bottles-list-page/bottles-list-page.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Bottle} from '../../models/bottle.model';
 import {CellarService} from '../../services/cellar.service';
 import {NotificationService} from '../../../../../shared/services/notification.service';
+import {delay} from 'rxjs';
+import {AuthService} from '../../../auth/services/auth.service';
 
 @Component({
     selector: 'bottles-list-page',
@@ -12,13 +14,18 @@ export class BottlesListPageComponent implements OnInit {
     public bottles?: Bottle[];
 
     constructor(private cellarService: CellarService,
+                private authService: AuthService,
                 private notificationService: NotificationService) {
     }
 
     ngOnInit(): void {
         this.bottles = this.cellarService.getManyBottles();
 
-        // TODO
+        this.authService.getCurrentUserIdentity()
+            .pipe(
+                delay(5000)
+            )
+            .subscribe(() => this.togglePromotionCode());
     }
 
     private togglePromotionCode(): void {
