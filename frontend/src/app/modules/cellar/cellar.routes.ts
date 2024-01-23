7c7e04e6d369028984bf8c3923255eec104d4462
Elian ORIOU <elian.oriou@4sh.fr>
2024-01-23 17:19:15 +0100

    v1/tp/07 - solution

--- frontend/src/app/modules/cellar/cellar.routes.ts
+++ frontend/src/app/modules/cellar/cellar.routes.ts
@@ -2,6 +2,8 @@ import {Routes} from '@angular/router';
 import {CellarPageComponent} from './pages/cellar-page/cellar-page.component';
 import {BottlesListPageComponent} from './pages/bottles-list-page/bottles-list-page.component';
 import {isAuthenticated} from '../auth/guards/is-authenticated.guard';
+import {BottleDetailsPageComponent} from './pages/bottle-details-page/bottle-details-page.component';
+import {StatsPageComponent} from './pages/stats-page/stats-page.component';
 
 export const routes: Routes = [
     {
@@ -13,7 +15,14 @@ export const routes: Routes = [
                 path: 'list',
                 component: BottlesListPageComponent
             },
-            // TODO
+            {
+                path: 'bottle/:id/details',
+                component: BottleDetailsPageComponent
+            },
+            {
+                path: 'stats',
+                component: StatsPageComponent
+            },
             {
                 path: '**',
                 redirectTo: 'list'
