af65acb81607d80957593343901fc5fc174343d4
fcoasnes <florian.coasnes@4sh.fr>
2026-06-16 14:15:19 +0200

    transform to signal inputs, outputs and needed properties

--- frontend/src/app/features/cellar/components/bottle-tile/bottle-tile.component.ts
+++ frontend/src/app/features/cellar/components/bottle-tile/bottle-tile.component.ts
@@ -1,19 +1,19 @@
-import {Component, Input} from '@angular/core';
-import {Bottle} from '../../models/bottle.model';
-import {BottleStickerImageComponent} from '../bottle-sticker-image/bottle-sticker-image.component';
-import {NgClass} from '@angular/common';
+import {Component, input} from ‘@angular/core’;
+import {Bottle} from ‘../../models/bottle.model’;
+import {BottleStickerImageComponent} from ‘../bottle-sticker-image/bottle-sticker-image.component’;
+import {NgClass} from ‘@angular/common’;
 
 @Component({
-    selector: 'bottle-tile',
-    templateUrl: './bottle-tile.component.html',
+    selector: ‘bottle-tile’,
+    templateUrl: ‘./bottle-tile.component.html’,
     imports: [
         BottleStickerImageComponent,
         NgClass
     ],
-    styleUrl: './bottle-tile.component.scss'
+    styleUrl: ‘./bottle-tile.component.scss’
 })
 export class BottleTileComponent {
-    @Input() public bottle?: Bottle;
+    public bottle = input<Bottle>();
 
     public onImageClick(bottle: Bottle): void {
         alert(`click sur l’image de la bouteille ${bottle.estate}`);
